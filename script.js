ff434f727ad6ec41150860cb7c148fb16deea379
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-08-20 11:26:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //your JS code here. If required.
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-bands.sort((a,b)=>a-b);
+function util(val) {
+	return val.replace(/(a |an |the)/i, "").trim();
+}
+
+bands.sort((a,b)=> util(a)>util(b)?1:-1);
 
 const ul = document.getElementById("band");
 
